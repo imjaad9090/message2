40373bf56669b8722f931c671704e4060e87c1fd
Imjaad <imjaadhaider@gmail.com>
2017-12-29 22:10:36 +0500

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protect_from_forgery with: :exception
+
+
+
+  protected
+
+  def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+   devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:username, :email, :password, :remember_me) }
+   devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password) }
+
+end
+end
---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+
+#  get 'users/show'
+
+  devise_for :users
+resources :posts do
+  resources :comments
+end
+root "posts#index"
+
+get '/about', to: 'pages#about'
+resources :users, :only => [:show]
+get 'users/:id' => 'users#show'
+get 'users' => 'users#index'
+
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
